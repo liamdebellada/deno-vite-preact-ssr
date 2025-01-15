dfacfab45b89f3ad291515e9268d49136aa18f0d
liamdebellada <liamdebell11@gmail.com>
2025-01-15 16:17:21 +0000

    Cleanup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # deno-vite-preact-ssr
+
 A Deno, Preact and Vite SSR project.
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { createRequire } from "node:module";
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 
 import deno from "@deno/vite-plugin";
 import preact from "@preact/preset-vite";
@@ -13,16 +13,12 @@ export default defineConfig({
     },
   },
   plugins: [
-    // deno-lint-ignore ban-ts-comment
-    // @ts-ignore
-    deno(),
-    // deno-lint-ignore ban-ts-comment
-    // @ts-ignore
+    deno() as PluginOption,
     preact({
       prefreshEnabled: false,
       babel: {
         cwd: createRequire(import.meta.url).resolve("@preact/preset-vite"),
       },
-    }),
+    }) as PluginOption,
   ],
 });
